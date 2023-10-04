832df59f05111feb0aeb3e49f9b8744c85cbdd41
macos15 <22384904@qq.com>
2023-10-04 08:47:45 +0800

    portable.h

--- src/portable.h
+++ src/portable.h
@@ -55,15 +55,6 @@ std::wstring GetUserDataDir()
 
     return temp;
 }
-std::wstring GetDiskCacheDir()
-{
-    std::wstring path = GetAppDir() + L"\\.\\Cache";
-
-    TCHAR temp[MAX_PATH];
-    ::PathCanonicalize(temp, path.data());
-
-    return temp;
-}
 
 // 构造新命令行
 std::wstring GetCommand(LPWSTR param)
